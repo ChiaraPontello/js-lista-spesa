3db9fa0196d87b8387e8b524ba00721b2982717e
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-16 17:10:51 +0200

    prompt

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const spesa = [];
+
+
+let stampa = document.getElementById('stampa');
+
+
+let prodotto ;
+
+let prodottoLista = 0;
+
+while (spesa != "stop"){
+    spesa = prompt("acquista prodotti");
+    
+    spesa.push(spesa);
+
+
+
+if (){
+
+   
+}}
